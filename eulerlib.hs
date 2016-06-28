e6ce0fb77987aac2eceab0a1bccfec7798e57423
Zach Easterbrook <FishLikeHexagon@gmail.com>
2016-06-28 16:55:06 -0400

    Added problem 8 and updated eulerlib.hs

--- eulerlib.hs
+++ eulerlib.hs
@@ -1,3 +1,12 @@
 module EulerLib where
 
+import Data.List
+
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+chunk :: Int -> [a] -> [[a]]
+chunk n = takeWhile ((== n) . length) . transpose . take n . iterate tail
+
+charToArray :: [Char] -> [Int]
+charToArray [] = []
+charToArray (x:xs) = (read [x]::Int) : (charToArray xs)
